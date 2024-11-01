5fb84496b21e2d1cf7f2e5589566f043a6a24159
kekaze <jroyceyanson@gmail.com>
2024-11-01 13:09:32 +0800

    :sparkles: created a controller for inventories

---
+++ Controllers/InventoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProcureToPay.Models;
+
+namespace ProcureToPay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoriesController : ControllerBase
+    {
+        private readonly ProcureToPayContext _dbContext;
+        public InventoriesController(ProcureToPayContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            _dbContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetInventories()
+        {
+            IQueryable<Inventory> inventories = _dbContext.Inventories;
+
+            return Ok(await inventories.ToArrayAsync());
+        }
+    }
+}
